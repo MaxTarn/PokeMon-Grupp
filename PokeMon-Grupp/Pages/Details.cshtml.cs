bd2f6d94031d27877058871eae6526715d5d6966
miiamole <miia_pelaez@hotmail.com>
2024-02-07 13:47:23 +0100

    lagt till på sidorna

--- PokeMon-Grupp/Pages/Details.cshtml.cs
+++ PokeMon-Grupp/Pages/Details.cshtml.cs
@@ -1,12 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PokeMon_Grupp.Model;
 
 namespace PokeMon_Grupp.Pages
 {
-    public class DetailsModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class DetailsModel : PageModel
+	{
+		public int MyProperty { get; set; }
+		public void OnGet(PokemonModel pokemon)
+		{
+
+		}
+	}
 }
