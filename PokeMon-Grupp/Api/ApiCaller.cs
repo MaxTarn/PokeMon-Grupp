872ecc49c440849cc22c7d155e463ffbb3a4a79b
miiamole <miia_pelaez@hotmail.com>
2024-02-07 11:14:41 +0100

    added classes

---
+++ PokeMon-Grupp/Api/ApiCaller.cs
@@ -0,0 +1,36 @@
+﻿namespace PokeMon_Grupp.Api
+{
+    public class ApiCaller
+    {
+
+        public HttpClient Client { get; set; }
+
+        public ApiCaller()
+        {
+            Client = new();
+            //Lägg till en basadress till klienten, eftersom alla calls komemr gå till den sidan.
+            Client.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon/");
+        }
+        public async Task<Root> MakeCall(string url)
+        {
+            HttpResponseMessage response = await Client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+
+                Root? result = JsonConvert.DeserializeObject<Root>(json);
+                if (result != null)
+                {
+                    return result;
+                }
+
+            }
+
+            throw new HttpRequestException();
+        }
+
+
+
+    }
+}
---
+++ PokeMon-Grupp/Pages/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PokeMon_Grupp.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
