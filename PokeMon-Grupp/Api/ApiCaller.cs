2bb1e78bf6ce91c6e9556dd7f765aadf018813ea
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-02-08 13:50:26 +0100

    AllPokemonsChosen page now displays each pokemon chosen and does not crash when user has not chosen a pokemon

--- PokeMon-Grupp/Api/ApiCaller.cs
+++ PokeMon-Grupp/Api/ApiCaller.cs
@@ -15,24 +15,7 @@ namespace PokeMon_Grupp.Api
             //Lägg till en basadress till klienten, eftersom alla calls komemr gå till den sidan.
             Client.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon/");
         }
-        //public async Task<Root> MakeCall(string url)
-        //{
-        //    HttpResponseMessage response = await Client.GetAsync(url);
 
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        string json = await response.Content.ReadAsStringAsync();
-
-        //        Root? result = JsonConvert.DeserializeObject<Root>(json);
-        //        if (result != null)
-        //        {
-        //            return result;
-        //        }
-
-        //    }
-
-        //    throw new HttpRequestException();
-        //}
 
 
 
