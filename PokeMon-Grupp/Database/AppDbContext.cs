aeb20eb6f6082ed148f5593f52679b61351428b5
miiamole <miia_pelaez@hotmail.com>
2024-02-08 14:41:23 +0100

    databas

---
+++ PokeMon-Grupp/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokeMon_Grupp.Model;
+
+namespace PokeMon_Grupp.Database
+{
+	public class AppDbContext : DbContext
+	{
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //denna konstruktorn har vi för att kunna finna connectionstring?
+		{
+
+		}
+		public DbSet<PokemonModel> Pokemon { get; set; }
+	}
+}
--- PokeMon-Grupp/Program.cs
+++ PokeMon-Grupp/Program.cs
@@ -1,16 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using PokeMon_Grupp.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
+//HÄR LÄGGER VI TILL 2 rader
+
+
+
+// Hämta connection string
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+// Lägg till context i dependency injection container
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
